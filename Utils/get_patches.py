acb05631d3fc5d7f1925f3ebed14e4f6aebf3777
Lance E Eaton <leato001@plattsburgh.edu>
2019-05-24 01:13:49 -0400

    get_patches now gets all patch data

--- Utils/get_patches.py
+++ Utils/get_patches.py
@@ -4,37 +4,38 @@ import requests
 import pandas as pd
 import csv
 from bs4 import BeautifulSoup
-'''
-opts = Options()
-opts.set_headless()
-'''
-browser = webdriver.Chrome('G:\\ChromeWebDriver\\chromedriver.exe')
-url ='https://wowwiki.fandom.com/wiki/Patches'
 
-browser.get(url)
-tables = browser.find_elements_by_class_name('sortable')
 write_csv = 'Data\\patch_data.csv'
 
-with open(write_csv, 'a', newline='') as csvFile: # adding header for easier use later
+urls = ['https://wowwiki.fandom.com/wiki/Patches'] # Initialized with this url as it doesn’t fit the pattern 
+
+for x in range(6,0,-1):
+  urls.append('https://wowwiki.fandom.com/wiki/Patches/{}.x'.format(x)) # getting all our urls
+
+browser = webdriver.Chrome('G:\\ChromeWebDriver\\chromedriver.exe')
+
+with open(write_csv, 'a', newline='') as csvFile: # adding header 
   writer = csv.writer(csvFile)
   writer.writerow(['Patch','Release Date','Version','Interface','Notes'])
 
+  for url in urls:
+    browser.get(url)
+    tables = browser.find_elements_by_class_name('sortable')
+    for table in tables:
+      soup = BeautifulSoup(table.get_attribute('innerHTML'), 'html.parser')
+      rows = soup.select('tbody tr')
+      for row in rows:
+        cells = row.select('td')
+        all_cell_values = []
+        for cell in cells:
+          notes = cell.find_all('li')
+          if len(notes) == 0:
+            all_cell_values.append(cell.text.strip())
+          else:
+            notes = [note.text.strip() for note in notes]
+            all_cell_values.append(notes)
 
-  for table in tables[0:1]:
-    soup = BeautifulSoup(table.get_attribute('innerHTML'), 'html.parser')
-    rows = soup.select('tbody tr')
-    for row in rows:
-      cells = row.select('td')
-      all_cell_values = []
-      for cell in cells:
-        notes = cell.find_all('li')
-        if len(notes) == 0:
-          all_cell_values.append(cell.text.strip())
-        else:
-          notes = [note.text.strip() for note in notes]
-          all_cell_values.append(notes)
-      writer.writerow(all_cell_values)
-      print(all_cell_values)
+        writer.writerow(all_cell_values)
 
 browser.close()
 
--- Utils/get_urls.py
+++ Utils/get_urls.py
@@ -4,38 +4,32 @@ import json
 import time
 import csv
 
-def get_pushshift_urls():
-    """
-    Gets all reddit urls and creation dates from the wow subreddit and writes them to a csv named urls.csv. 
+after_date = 1217611716
+before_date = int(time.time())
+columns = ['Created Date','Full Url']
+write_file = 'Data\\urls.csv'
 
-    """
-    after_date = 1217611716
-    before_date = int(time.time())
-    columns = ['Created Date','Full Url']
-    write_file = 'Data\\urls.csv'
-    
-    with open(write_file, 'a', newline='') as csvFile: # adding header to file
-      writer = csv.writer(csvFile)
-      writer.writerow(columns)
-    
-    while True:
-      url = 'https://api.pushshift.io/reddit/search/submission/?subreddit=wow&sort=asc&sort_type=created_utc&after='+str(after_date)+'&before='+str(before_date)+'&size=1000'
-      response = requests.get(url)
-      data = json.loads(response.content)['data']
-      
-      if len(data) != 0: # As long as there is data
-        page_df = pd.DataFrame()
-        
-        for i in range(len(data)): # get info from all posts on page
-            created_utc = data[i].get('created_utc')
-            full_link = data[i].get('full_link')
-            row_df = pd.DataFrame([[created_utc, full_link]],columns = columns) # one url, creation date pair
-            page_df = page_df.append(row_df, ignore_index = True) # all pairs on the page
-        
-        page_df.to_csv(write_file, mode='a', header = False, index = False) # don't include index or headings
-        after_date = int(page_df.tail(1)['Created Date'].values) + 1 
-        time.sleep(.5) # waiting half second after each request 
-      else: # no data
-        break
+with open(write_file, 'a', newline='') as csvFile: # adding header to file
+  writer = csv.writer(csvFile)
+  writer.writerow(columns)
 
-get_pushshift_urls()
+while True:
+  url = 'https://api.pushshift.io/reddit/search/submission/?subreddit=wow&sort=asc&sort_type=created_utc&after='+str(after_date)+'&before='+str(before_date)+'&size=1000'
+  response = requests.get(url)
+  data = json.loads(response.content)['data']
+  
+  if len(data) != 0: # As long as there is data
+    page_df = pd.DataFrame()
+    
+    for i in range(len(data)): # get info from all posts on page
+        created_utc = data[i].get('created_utc')
+        full_link = data[i].get('full_link')
+        row_df = pd.DataFrame([[created_utc, full_link]],columns = columns) # one url, creation date pair
+        page_df = page_df.append(row_df, ignore_index = True) # all pairs on the page
+    
+    page_df.to_csv(write_file, mode='a', header = False, index = False) # don't include index or headings
+    after_date = int(page_df.tail(1)['Created Date'].values) + 1 
+    time.sleep(.5) # waiting half second after each request 
+  else: # no data
+    break
+
